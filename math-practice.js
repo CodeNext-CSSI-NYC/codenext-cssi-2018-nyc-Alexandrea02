916f4b7526ade6e5b41bb50fa7ca61390c6d16d3
Alexandrea02 <35402360+Alexandrea02@users.noreply.github.com>
2018-07-09 17:00:42 -0400

    part 1 of easter prediction

--- math-practice.js
+++ math-practice.js
@@ -1,4 +1,5 @@
-// Under each question, write the code that solves it, and then write your answer in a comment
+// Author: Alexandrea Jackson 
+//Under each question, write the code that solves it, and then write your answer in a comment
 // For example, if the question is: What's the sum of 79 and 2018 you would write:
 console.log(79 + 2018);
 // 2097
@@ -7,17 +8,36 @@ console.log(79 + 2018);
 const p = 12;
 const q = 60;
 const r = 25;
+num;
 
 // 1. Is 91 divisible by 13? (The answer should be either true or false)
+console.log(91%13);
+//
+let num = 91%13;
 
 // 2. What is the result when the q divided by r is rounded to the nearest integer?
+console.log(q/r)
+//
+
 
 // 3. Is the product of p and r greater than q squared? (The answer should be either true or false)
+console.log(p*r); console.log(q**2);
+//
 
 // 4. What is the remainder when the sum of q and r is divided by p?
+console.log(q+r%p);
+//
 
 // 5. 27 squared is equal to what number raised to the third power?
+console.log(27**2=%3);
+//
 
 // 6. What is the absolute value of p - q? (Find out how to do absolute value by searching online.)
+Math.abs(p-q); 
+//
+
 
 // 7. Find another math operation we haven't talked about yet on the MDN (Mozilla Developer Network) and try it out below.
+console.log()
+Math.abs(endless);
+//
