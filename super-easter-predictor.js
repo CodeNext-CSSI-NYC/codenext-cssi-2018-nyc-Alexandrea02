efafa10983698c0e09b5572241337f270cb27407
Alexandrea02 <35402360+Alexandrea02@users.noreply.github.com>
2018-07-10 12:10:07 -0400

    easter day predictor

--- super-easter-predictor.js
+++ super-easter-predictor.js
@@ -1,31 +1,27 @@
 // Author: FirstName LastName
+let readline = require("readline-sync");
 
-a = 8;
-b = 20;
-c = 3;
-d = 5;
-e = 0;
-f = 1;
-g = 6;
-h = 26;
-i = 0;
-k = 3;
-r = 3;
-m = 0;
-n = 4;
-p = 19;
+console.log("Welcome to the Easter day identifier");
 
-y = 2002;
+let y= readline.question("please pick a year after 1583: ");
+console.log(y);
 
-Divide y by 19 and call the remainder a. Ignore the quotient.
-Divide y by 100 and get a quotient b and remainder c.
-Divide b by 4 and get quotient d and remainder e.
-Divide b + 8 by 25 and get quotient f. Ignore the remainder.
-Divide b - f + 1 by 3 and get quotient g. Ignore the remainder.
-Divide 19 * a + b - d - g + 15 by 30 and get remainder h. Ignore the quotient.
-Divide c by 4 and get quotient i and remainder k.
-Divide 32 + 2 * e + 2 * i - h - k by 7 and get remainder r. Ignore the quotient.
-Divide a + 11 * h + 22 * r by 451 and get quotient m. Ignore the remainder.
-Divide h + r - 7 * m + 114 by 31 and get quotient n and remainder p.
-The value of n gives the month (3 for March and 4 for April) and the value of p + 1 gives the day of the month. For example, if y is 2003:
-
+let a = y % 19;
+let b = Math.floor(y / 100);
+let c = y % 100;
+// console.log(y, a, b, c);
+let d = Math.floor(b / 4);
+let e = b % 4;
+let f = Math.floor((b + 8) / 25);
+let g = Math.floor((b - f + 1) / 3);
+let h = (19 * a + b - d - g + 15) % 30;
+let i = Math.floor(c / 4);
+let k = c % 4;
+let r = (32 + 2 * e + 2 * i - h - k) % 7;
+let m = Math.floor((a + 11 * h + 22 * r) / 451);
+let n = Math.floor((h + r - 7 * m + 114) / 31);
+let p = (h + r - 7 * m + 114) % 31;
+let day = p + 1;
+let month = n;
+console.log("This year Easter is on " + month + "/" + day);
+console.log("thank you for using the Easter Day identifier");
