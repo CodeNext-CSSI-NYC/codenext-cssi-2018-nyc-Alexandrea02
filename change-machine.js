e9f01182e7864dbd81cef37c14f7f994ec984a6a
Alexandrea02 <35402360+Alexandrea02@users.noreply.github.com>
2018-07-10 12:57:10 -0400

    first attempt

--- change-machine.js
+++ change-machine.js
@@ -1,1 +1,16 @@
-// Author: FirstName LastName
+// Author: Alexandrea Jackson 
+readline = require("readline-sync");
+console.log("hello welcome to the change converter");
+
+let x = readline.question("please enter your amount of cents: ");
+
+let pennies= Math.floor(x%100);
+let quarters = Math.floor(x/25);
+let dimes= Math.floor(x/10);
+let nickles= Math.floor(x/5);
+
+console.log("If you have " + x + " cents then you will recieve "); 
+console.log(pennies + " pennies");
+console.log(nickles + " nickles");
+console.log(dimes + " dimes");
+console.log(quarters + " quarters");
